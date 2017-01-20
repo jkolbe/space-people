bb635f8b956b2185be5f7a115c2869f0d0f2222d
Joanna Kolbe <joannakolbe@gmail.com>
2017-01-20 09:30:47 -0500

    basic API call

--- src/containers/people-container.js
+++ src/containers/people-container.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
 
+import People from '../components/people-list';
+
 export default class PeopleContainer extends Component {
+
+	constructor() {
+		super();
+		this.state = {
+			people: [],
+			number: 0
+		};
+	}
+
+	componentDidMount() {
+		$.get('http://api.open-notify.org/astros.json').then(
+			(res) => {
+				this.setState({people:res.people, number:res.number})
+				console.log(this.state);
+			}
+		);
+	}
+
 	render() {
 		return (
 			<div>
-			<h2>people container</h2>
+				<h2>people container</h2>
+				{this.state.people.map((p) => (<div>{p.name}</div>))}
 			</div>
 		);
 	}
